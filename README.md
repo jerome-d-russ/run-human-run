446c5b3188a63ccf0324902c0e7060f1bbe74655
Jerome Russ <jerome.d.russ+github@gmail.com>
2017-02-16 09:14:56 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+clone
+npm install
+npm install bower -g (if not already)
+bower install
+npm start
---
+++ index.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('node_modules'));
+app.use(express.static('public'));
+app.use(express.static('bower_components/vjoy'));
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+var server;
+io.on('connection', function(socket){
+  console.log('a user connected: ' + socket.id);
+  if(server){
+    server.emit('new user', socket.id);
+  }
+  socket.on('disconnect', function(){
+    console.log('disconnect');
+    if(server && socket.id == server.id){
+      console.log('server disconnect');
+      server = null;
+    } else {
+      if(server){
+        server.emit('remove', socket.id);
+      }
+    }
+  });
+  socket.on('server', function(msg){
+    if(!server){
+      console.log('not server!');
+      server = socket;
+      server.emit('server acknowledge', {});
+    } else {
+      console.log('else');
+    }
+  });
+  socket.on('move', function(msg){
+    if(server){
+      msg.id = socket.id;
+      server.emit('move', msg);
+    }
+  });
+  socket.on('invalid', function(sId){
+    if (io.sockets.sockets[sId]) {
+      console.log('disconnecting: ' + sId);
+      io.sockets.sockets[sId].disconnect();
+    }
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
